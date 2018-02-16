906f65c9f50bc9dfa26ebad3508682220c48b101
SIVA GANESH <sivaganesh@northwesternmutual.com>
2018-02-16 14:40:19 -0600

    latest exercise

--- PlanarNN/PlanarNN.py
+++ PlanarNN/PlanarNN.py
@@ -17,3 +17,28 @@ from planar_utils import plot_decision_boundary, sigmoid, load_planar_dataset, l
 #%matplotlib inline
 
 np.random.seed(1) # set a seed so that the results are consistent
+X, Y = load_planar_dataset()
+
+# Visualize the data:
+plt.scatter(X[0, :], X[1, :], c=Y, s=40, cmap=plt.cm.Spectral);
+
+### START CODE HERE ### (≈ 3 lines of code)
+shape_X = X.shape
+shape_Y = Y.shape
+m = X.shape[1]  # training set size
+### END CODE HERE ###
+
+print ('The shape of X is: ' + str(shape_X))
+print ('The shape of Y is: ' + str(shape_Y))
+print ('I have m = %d training examples!' % (m))
+# Train the logistic regression classifier
+clf = sklearn.linear_model.LogisticRegressionCV();
+clf.fit(X.T, Y.T);
+# Plot the decision boundary for logistic regression
+plot_decision_boundary(lambda x: clf.predict(x), X, Y)
+plt.title("Logistic Regression")
+
+# Print accuracy
+LR_predictions = clf.predict(X.T)
+print ('Accuracy of logistic regression: %d ' % float((np.dot(Y,LR_predictions) + np.dot(1-Y,1-LR_predictions))/float(Y.size)*100) +
+       '% ' + "(percentage of correctly labelled datapoints)")
